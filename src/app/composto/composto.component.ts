a037b7d3c832f22ab3c11778339e881ce25ee01e
elisama-maia <elisama_maia@hotmail.com>
2022-03-18 21:11:16 -0300

    Initial commit

---
+++ src/app/composto/composto.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-composto',
+  templateUrl: './composto.component.html',
+  styleUrls: ['./composto.component.css'],
+})
+export class CompostoComponent implements OnInit {
+  valor: number = undefined;
+  taxa: number = undefined;
+  periodo: number = undefined;
+  total: number = 0;
+  juros: number = undefined;
+
+  constructor() {
+    this.valor = 1000;
+    this.taxa = 1;
+    this.periodo = 6;
+  }
+
+  ngOnInit() {}
+
+  jurosTotal() {
+    return this.valorTotal() - this.valor;
+  }
+
+  valorTotal() {
+    return this.valorTotalParcial(this.periodo);
+  }
+
+  valorTotalParcial(periodo: number) {
+    return this.valor * Math.pow(1 + this.taxa / 100, periodo);
+  }
+
+  parcelas() {
+    let list: number[] = [];
+    for (var i = 1; i <= this.periodo; i++) {
+      list.push(this.valorTotalParcial(i));
+    }
+    return list;
+  }
+}
---
+++ src/app/simples/simples.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simples',
+  templateUrl: './simples.component.html',
+  styleUrls: ['./simples.component.css'],
+})
+export class SimplesComponent implements OnInit {
+  valor: number;
+  taxa: number;
+  periodo: number;
+  juros: number = undefined;
+  totaljuros: number = undefined;
+  valortotal: number = undefined;
+
+  constructor() {
+    this.valor = 1000;
+    this.taxa = 1;
+    this.periodo = 6;
+  }
+
+  ngOnInit() {}
+
+  jurosSimples() {
+    let list: number[] = [];
+    this.totaljuros = ((this.valor * this.taxa) / 100) * this.periodo;
+    this.valortotal = this.valor + this.totaljuros;
+    for (let i = 1; i <= this.periodo; i++) {
+      this.juros = (this.valor * this.taxa) / 100;
+      list.push(this.juros);
+    }
+    return list;
+  }
+}
